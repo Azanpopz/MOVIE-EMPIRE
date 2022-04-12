337abe5781929801312dec2fbe272ddee726cf9e
Azanpopz <89448511+Azanpopz@users.noreply.github.com>
2022-04-12 18:22:13 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,10 +1,10 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/200920082007/TigerShroff.git /TigerShroff
+  git clone https://telegra.ph/file/595e6b626b2fee08397d6.jpg)](https://heroku.com/deploy?template=https://github.com/Azanpopz/MOVIE-EMPIRE.git /MOVIE-EMPIRE
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
-  git clone $UPSTREAM_REPO /Tigershroff
+  git clone $UPSTREAM_REPO /MOVIE-EMPIRE
 fi
 cd /Tigershroff
 pip3 install -U -r requirements.txt
